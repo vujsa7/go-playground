2374c1b5efc6b4ba52ffafb635217c0b78c6b031
Aleksa Vujisić <61715001+vujsa7@users.noreply.github.com>
2025-05-17 13:31:19 +0200

    chore: First commit

---
+++ inputoutput/go.mod
@@ -0,0 +1,3 @@
+module aleksavujisic/test
+
+go 1.22.5
---
+++ inputoutput/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	const maxSpeed byte = 60
+	message := "Hello from module!"
+	price := 34.4
+
+	print(message, price, maxSpeed)
+
+}
